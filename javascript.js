cefc364a61cdf303bb6f432df50e7c0069cc541f
lukeform <lukethomascgt@gmail.com>
2024-04-18 14:47:49 +0100

    Now has user interactions for grid size pushing before attempting extra credit

--- javascript.js
+++ javascript.js
@@ -1,292 +1,59 @@
+let box;
+let row;
+
 
 const newGrid = document.querySelector("#newGrid")
 newGrid.addEventListener("click", () =>{
-        let gridSize =prompt("How big da grid (No more than 100 pls");
-        console.log(gridSize)
+
+        destroyGrid();
+        createGrid();
 
 });
 
-const container =  document.querySelector("#container");
-
-const row1 = document.createElement("div");
-row1.classList.add("row1");
-row1.style.backgroundColor = "blue";
-
-
-container.appendChild(row1);
-
-        const box1 = document.createElement("div");
-        box1.classList.add("box1");
-        box1.style.backgroundColor = "orange";
-        box1.textContent= "i'm box 1";
-
-            box1.addEventListener("mouseover", () => {
-                box1.style.backgroundColor = "red";
-            });
-
-            box1.addEventListener("mouseout", () =>{
-                box1.style.backgroundColor = "red";
-
-            });
-
-        row1.appendChild(box1);
-
-        const box2 = document.createElement("div");
-        box2.classList.add("box2");
-        box2.style.backgroundColor = "orange";
-        box2.textContent= "i'm box 2";
-
-            box2.addEventListener("mouseover", () => {
-                box2.style.backgroundColor = "red";
-            });
-
-            box2.addEventListener("mouseout", () =>{
-                box2.style.backgroundColor = "red";
-
-            });
-
-        row1.appendChild(box2);
-
-        const box3 = document.createElement("div");
-        box3.classList.add("box3");
-        box3.style.backgroundColor = "orange";
-        box3.textContent= "i'm box 3";
-
-            box3.addEventListener("mouseover", () => {
-                box3.style.backgroundColor = "red";
-            });
-
-            box3.addEventListener("mouseout", () =>{
-                box3.style.backgroundColor = "red";
-
-            });
-
-        row1.appendChild(box3);
-
-        const box4 = document.createElement("div");
-        box4.classList.add("box4");
-        box4.style.backgroundColor = "orange";
-        box4.textContent= "i'm box 4";
-
-            box4.addEventListener("mouseover", () => {
-                box4.style.backgroundColor = "red";
-            });
-
-            box4.addEventListener("mouseout", () =>{
-                box4.style.backgroundColor = "red";
-
-            });
-
-        row1.appendChild(box4);
-
-const row2 = document.createElement("div");
-row2.classList.add("row2");
-row2.style.backgroundColor = "red";
-
-
-container.appendChild(row2);
-
-        const box5 = document.createElement("div");
-        box5.classList.add("box5");
-        box5.style.backgroundColor = "orange";
-        box5.textContent= "i'm box 5";
-
-            box5.addEventListener("mouseover", () => {
-                box5.style.backgroundColor = "red";
-            });
-
-            box5.addEventListener("mouseout", () =>{
-                box5.style.backgroundColor = "red";
-
-            });
-
-        row2.appendChild(box5);
-
-        const box6 = document.createElement("div");
-        box6.classList.add("box6");
-        box6.style.backgroundColor = "orange";
-        box6.textContent= "i'm box 6";
-
-            box6.addEventListener("mouseover", () => {
-                box6.style.backgroundColor = "red";
-            });
-
-            box6.addEventListener("mouseout", () =>{
-                box6.style.backgroundColor = "red";
-
-            });
-
-        row2.appendChild(box6);
-
-        const box7 = document.createElement("div");
-        box7.classList.add("box7");
-        box7.style.backgroundColor = "orange";
-        box7.textContent= "i'm box 7";
-
-            box7.addEventListener("mouseover", () => {
-                box7.style.backgroundColor = "red";
-            });
-
-            box7.addEventListener("mouseout", () =>{
-                box7.style.backgroundColor = "red";
-
-            });
-
-        row2.appendChild(box7);
-
-        const box8 = document.createElement("div");
-        box8.classList.add("box8");
-        box8.style.backgroundColor = "orange";
-        box8.textContent= "i'm box 8";
-
-            box8.addEventListener("mouseover", () => {
-                box8.style.backgroundColor = "red";
-            });
-
-            box8.addEventListener("mouseout", () =>{
-                box8.style.backgroundColor = "red";
-
-            });
-
-        row2.appendChild(box8);
-
-const row3 = document.createElement("div");
-row3.classList.add("row3");
-row3.style.backgroundColor = "green";
-
-
-container.appendChild(row3);
-
-        const box9 = document.createElement("div");
-        box9.classList.add("box9");
-        box9.style.backgroundColor = "orange";
-        box9.textContent= "i'm box 9";
-
-            box9.addEventListener("mouseover", () => {
-                box9.style.backgroundColor = "red";
-            });
-
-            box9.addEventListener("mouseout", () =>{
-                box9.style.backgroundColor = "red";
-
-            });
-
-        row3.appendChild(box9);
-
-        const box10 = document.createElement("div");
-        box10.classList.add("box10");
-        box10.style.backgroundColor = "orange";
-        box10.textContent= "i'm box 10";
-
-            box10.addEventListener("mouseover", () => {
-                box10.style.backgroundColor = "red";
-            });
-
-            box10.addEventListener("mouseout", () =>{
-                box10.style.backgroundColor = "red";
-
-            });
-
-        row3.appendChild(box10);
-
-        const box11 = document.createElement("div");
-        box11.classList.add("box11");
-        box11.style.backgroundColor = "orange";
-        box11.textContent= "i'm box 11";
-
-            box11.addEventListener("mouseover", () => {
-                box11.style.backgroundColor = "red";
-            });
-
-            box11.addEventListener("mouseout", () =>{
-                box11.style.backgroundColor = "red";
-
-            });
-
-        row3.appendChild(box11);
-
-        const box12 = document.createElement("div");
-        box12.classList.add("box12");
-        box12.style.backgroundColor = "orange";
-        box12.textContent= "i'm box 12";
-
-            box12.addEventListener("mouseover", () => {
-                box12.style.backgroundColor = "red";
-            });
-
-            box12.addEventListener("mouseout", () =>{
-                box12.style.backgroundColor = "red";
-
-            });
-
-        row3.appendChild(box12);
-
-const row4 = document.createElement("div");
-row4.classList.add("row4");
-row4.style.backgroundColor = "yellow";
-
-container.appendChild(row4);
-
-        const box13 = document.createElement("div");
-        box13.classList.add("box13");
-        box13.style.backgroundColor = "orange";
-        box13.textContent= "i'm box 13";
-
-            box13.addEventListener("mouseover", () => {
-                box13.style.backgroundColor = "red";
-            });
-
-            box1.addEventListener("mouseout", () =>{
-                box13.style.backgroundColor = "red";
-
-            });
-
-        row4.appendChild(box13);
-
-        const box14 = document.createElement("div");
-        box14.classList.add("box14");
-        box14.style.backgroundColor = "orange";
-        box14.textContent= "i'm box 14";
-
-            box14.addEventListener("mouseover", () => {
-                box14.style.backgroundColor = "red";
-            });
-
-            box14.addEventListener("mouseout", () =>{
-                box14.style.backgroundColor = "red";
-
-            });
-
-        row4.appendChild(box14);
-
-        const box15 = document.createElement("div");
-        box15.classList.add("box15");
-        box15.style.backgroundColor = "orange";
-        box15.textContent= "i'm box 15";
-
-            box15.addEventListener("mouseover", () => {
-                box15.style.backgroundColor = "red";
-            });
-
-            box15.addEventListener("mouseout", () =>{
-                box15.style.backgroundColor = "red";
-
-            });
-
-        row4.appendChild(box15);
-
-        const box16 = document.createElement("div");
-        box16.classList.add("box16");
-        box16.style.backgroundColor = "orange";
-        box16.textContent= "i'm box 16";
-
-            box16.addEventListener("mouseover", () => {
-                box16.style.backgroundColor = "red";
-            });
-
-            box16.addEventListener("mouseout", () =>{
-                box16.style.backgroundColor = "red";
-
-            });
-
-        row4.appendChild(box16);
+function destroyGrid(){
+    // const rowParent = document.querySelector("#container");
+    // const rowChild = document.querySelectorAll(".row");     
+    // const boxChild = document.querySelectorAll(".box");   
+    // console.log(rowChild);
+    // console.log(boxChild);
+    // console.log(rowChild.length);
+    
+    // while (rowChild.length > 0){
+
+    //     console.log("while is being read");
+        
+        document.querySelector("#container").innerHTML = "";
+
+
+    
+}
+
+function createGrid(){
+    let gridSize = prompt("Please choose a grid size (Less than 100 recommended)");
+    console.log(gridSize)
+    
+    const container =  document.querySelector("#container");
+
+
+    for(i=0; i < gridSize; i++){
+        
+        let row = document.createElement("div");
+        row.classList.add("row");
+        row.style.backgroundColor = "blue";
+        textContent = "im a row";
+        container.appendChild(row);
+            for (x=0; x < gridSize; x++){
+
+                let box = document.createElement("div");
+                box.classList.add("box");
+                box.style.backgroundColor = "orange";     
+                row.appendChild(box);
+                box.addEventListener("mouseover", () => {
+                box.style.backgroundColor = "red";});
+            };
+         }
+    
+         box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = "red";});
+        
+        }
